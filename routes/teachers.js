1223db5bcc018c83d347112a4c7836d8577680d1
Fran <frangarlop@gmail.com>
2021-03-31 09:15:04 +0200

    modificacion vistas teachers

--- routes/teachers.js
+++ routes/teachers.js
@@ -10,6 +10,20 @@ router.get('/', (req, res) => {
         .catch(error => console.log(error));
 });
 
+// GET /teachers/nuevo
+router.get('/new', (req, res) => {
+    res.render('teachers/new');
+});
+
+router.post('/create', (req, res) => {
+    Teacher.create(req.body)
+        .then(nuevoTeacher => {
+            console.log(nuevoTeacher);
+            res.redirect('/teachers');
+        })
+        .catch(error => console.log(error));
+});
+
 // GET /teachers/editar/IDTEACHER
 router.get('/editar/:idTeacher', (req, res) => {
     Teacher.findById(req.params.idTeacher)
@@ -22,6 +36,17 @@ router.post('/update', (req, res) => {
         .then(teacherEditado => res.redirect('/teachers'))
         .catch(error => console.log(error))
 });
+
+// GET /teachers/delete/IDTEACHER
+router.get('/delete/:idTeacher', (req, res) => {
+    Teacher.findByIdAndDelete(req.params.idTeacher)
+        .then(teacherBorrado => {
+            console.log(teacherBorrado);
+            res.redirect('/teachers');
+        })
+        .catch(error => console.log(error));
+});
+
 
 
 module.exports = router;
